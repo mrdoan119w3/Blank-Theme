ac90ac8c3e922730230eeeec4a4674b021eff6ac
UyDoan <mrdoan119w3@gmail.com>
2018-05-04 10:00:03 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="footer" itemscope="" itemtype="http://schema.org/WPFooter">
+  		<div class="container">
+  			<div class="footer_wrap">
+  			<!-- Footer code in here -->
+  			</div>
+  		</div>
+  	</footer>
+    <div class="over_wrap"></div>
+  	<?php wp_footer(); ?>
+  	</body>
+</html>
